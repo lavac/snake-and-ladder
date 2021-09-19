5e60eef3661bf3be2895cd29d47ece8a0a5b49de
Lavanya C <lavanyacs743@gmail.com>
2021-09-19 10:48:17 +0530

    Story2 - Lavanya | Handle InvalidSnakeStartingPositionException and InvalidSnakeEndingPositionException exceptions

--- src/main/java/com/games/snakeandladder/SnakeAndLadderApplication.java
+++ src/main/java/com/games/snakeandladder/SnakeAndLadderApplication.java
@@ -4,6 +4,8 @@ import com.games.snakeandladder.entity.Dice;
 import com.games.snakeandladder.entity.Player;
 import com.games.snakeandladder.entity.Snake;
 import com.games.snakeandladder.entity.SnakeAndLadderBoard;
+import com.games.snakeandladder.exception.InvalidSnakeEndingPositionException;
+import com.games.snakeandladder.exception.InvalidSnakeStartingPositionException;
 
 import java.util.Scanner;
 
@@ -11,18 +13,23 @@ public class SnakeAndLadderApplication {
 
   public static void main(String[] args) {
     final int DEFAULT_SIZE = 100;
-
-    Scanner scanner = new Scanner(System.in);
-    int startingIndex = scanner.nextInt();
-    int endingIndex = scanner.nextInt();
-    Snake snake = new Snake();
-    snake.setStartingPosition(startingIndex);
-    snake.setEndingPosition(endingIndex);
-    SnakeAndLadderBoard snakeAndLadderBoard = new SnakeAndLadderBoard(DEFAULT_SIZE);
-    snakeAndLadderBoard.addSnake(snake);
-    Player player = new Player("Player1");
-    Dice dice = new Dice();
-    SnakeAndLadderGame game = new SnakeAndLadderGame(snakeAndLadderBoard, dice, player);
-    game.start();
+    try {
+      Scanner scanner = new Scanner(System.in);
+      int startingIndex = scanner.nextInt();
+      int endingIndex = scanner.nextInt();
+      Snake snake = new Snake();
+      snake.setStartingPosition(startingIndex);
+      snake.setEndingPosition(endingIndex);
+      SnakeAndLadderBoard snakeAndLadderBoard = new SnakeAndLadderBoard(DEFAULT_SIZE);
+      snakeAndLadderBoard.addSnake(snake);
+      Player player = new Player("Player1");
+      Dice dice = new Dice();
+      SnakeAndLadderGame game = new SnakeAndLadderGame(snakeAndLadderBoard, dice, player);
+      game.start();
+    } catch (InvalidSnakeStartingPositionException ex) {
+      System.out.println("Starting position of the snake should be greater than zero and less than the board size");
+    } catch (InvalidSnakeEndingPositionException ex) {
+      System.out.println("Ending position should be less than the starting position and greater than or equal to zero");
+    }
   }
 }
