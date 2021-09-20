d853cb42efb38134b2c2b7af9a0c2ef9c170f35d
Lavanya C <lavanyacs743@gmail.com>
2021-09-20 09:11:19 +0530

    Story3 - Lavanya | Add console messages to read the input and add catch block to handle unexpected error

--- src/main/java/com/games/snakeandladder/SnakeAndLadderApplication.java
+++ src/main/java/com/games/snakeandladder/SnakeAndLadderApplication.java
@@ -13,23 +13,32 @@ public class SnakeAndLadderApplication {
   public static void main(String[] args) {
     try {
       Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Enter Snake's start position (from 1 to 99) and end position (less than start position)");
       int startPosition = scanner.nextInt();
       int endPosition = scanner.nextInt();
+
       Snake snake = new Snake();
       snake.setStartPosition(startPosition);
       snake.setEndPosition(endPosition);
       SnakeAndLadderBoard snakeAndLadderBoard = new SnakeAndLadderBoard(DEFAULT_SIZE);
       snakeAndLadderBoard.addSnake(snake);
       Player player = new Player(PLAYER_NAME);
+
+      System.out.println("Enter Dice type: Valid values are Straight and Crooked");
       String diceTypeInString = scanner.next();
+
       DiceType diceType = DiceType.getDiceType(diceTypeInString);
       Dice dice = DiceFactory.get(diceType);
       SnakeAndLadderGame snakeAndLadderGame = new SnakeAndLadderGame(snakeAndLadderBoard, dice, player);
+
       snakeAndLadderGame.start();
     } catch (InvalidSnakeStartPositionException
         | InvalidSnakeEndPositionException
         | InvalidDiceTypeException exception) {
       System.out.println(exception.getMessage());
+    } catch (Exception ex) {
+      System.out.println("Unexpected error occurred with message: " + ex.getMessage());
     }
   }
 }
